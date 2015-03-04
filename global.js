0e68929c4da04d65b97a010dabafbd5eb423c22b
Andrew Yolland <and.t.yoll@gmail.com>
2015-03-04 15:15:43 -0600

    more questions

--- global.js
+++ global.js
@@ -1,7 +1,16 @@
 var score = 0;
 var question1 = { question: "A dog says what?", goodAnswer: "bark", badAnswers: ["quack", "moo", "meow"]};
 var question2 = { question: "Vermillion is what color?", goodAnswer: "red", badAnswers: ["blue","yellow","green"]};
-var $questions = [question1,question2];
+var question3 = {question: "Who hasn't Batman fought?", goodAnswer: "Rocket Raccoon", badAnswers: ["Predator","Capt America","Judge Dredd"]};
+var question4 = {question: "Who Directed 'Jaws'?", goodAnswer: "Steven Spielberg", badAnswers: ["David Fincher","James Cameron","Chris Columbus"]};
+var question5 = {question: "What is 11 squared?", goodAnswer: "121", badAnswers: ["2","126","92"]};
+var question6 = {question: "Which artist was an impressionist?", goodAnswer: "Monet", badAnswers: ["DaVinci","Klimpt","Warhol"]};
+var question7 = {question: "Where does it hurt?", goodAnswer: "Everywhere", badAnswers: ["David Fincher","My foot","I don't know."]};
+var question8 = {question: "Which book is not by J.R.R. Tolkien?", goodAnswer: "The Wheel of Time", badAnswers: ["The Simarillion","Farmer Giles of Ham","The Two Towers"]};
+var question9 = {question: "In Javascript: (56 / (3 - 3))", goodAnswer: "Infinity", badAnswers: ["Nil","0.0000400501","NaN"]};
+var question0 = {question: "GWUH?", goodAnswer: "wat", badAnswers: ["BLUH","HEYO","FLURB"]};
+var $questions = [question1,question2,question3,question4,question5,question6,question7,question8,question9,question0];
+
                   
 function displayQuestion(question){
   var answers = randomize(question.badAnswers.concat(question.goodAnswer));
