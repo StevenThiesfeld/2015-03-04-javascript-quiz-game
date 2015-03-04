3d9f777d69cf74c9973dc1b10386e9739d80951f
Andrew Yolland <and.t.yoll@gmail.com>
2015-03-04 14:43:02 -0600

    loops game

--- global.js
+++ global.js
@@ -1,5 +1,7 @@
 var score = 0;
 var question1 = { question: "A dog says what?", goodAnswer: "bark", badAnswers: ["quack", "moo", "meow"]};
+var question2 = { question: "Vermillion is what color?", goodAnswer: "red", badAnswers: ["blue","yellow","green"]};
+var $questions = [question1,question2];
                   
 function displayQuestion(question){
   var answers = randomize(question.badAnswers.concat(question.goodAnswer));
@@ -9,8 +11,25 @@ function displayQuestion(question){
 }
 
 function getAnswer(question){
-  
   var answer = prompt(displayQuestion(question));
+  checkAnswer(answer,question.goodAnswer);
+}
+
+function checkAnswer(answer,rightAnswer){
+  if (answer === rightAnswer) {
+    score++;
+    alert("That answer is correct!");
+  } else {
+    alert("Sorry, That is incorrect.");
+  }
+}
+
+function playGame(){
+  var temp = randomize($questions)
+  for (z = 0; z < $questions.length; z++){
+    getAnswer(temp[z]);
+  }
+  return score;
 }
 
 function randomize(array){
@@ -18,3 +37,5 @@ function randomize(array){
      return Math.random() - .5 ;
   } );
 }
+
+playGame();
